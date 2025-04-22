dc855b9169d2c748085113b4ac2d5e6ef91a3531
Felix Benedictus Setiawan <123315666+CrowZF01@users.noreply.github.com>
2025-04-22 23:31:08 +0700

    Create Minggu8_Tugas_Mandiri_1.1.py

---
+++ Minggu8_Tugas_Mandiri_1.1.py
@@ -0,0 +1,14 @@
+def anagram(kata_1,kata_2):
+  kata_1 = kata_1.lower().replace(' ', '')
+  kata_2 = kata_2.lower().replace(' ', '')
+
+  if len(kata_1) != len(kata_2):
+    return 'tidak sama jumlah hurufnya'
+  
+  for i in kata_1:
+    if kata_1.count(i) != kata_2.count(i):
+      return f'tidak sama hurufnya'
+    else:
+      return f'kata tersebut anagram'
+
+print(anagram("atma", "tama"))
