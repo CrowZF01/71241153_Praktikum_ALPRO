0d7e40afdee070514872fcd982c8d3f29c9ae9c2
Felix Benedictus Setiawan <123315666+CrowZF01@users.noreply.github.com>
2025-04-30 18:44:32 +0700

    Update Minggu10_Tugas_Mandiri_1.2.py

--- Minggu10_Tugas_Mandiri_1.2.py
+++ Minggu10_Tugas_Mandiri_1.2.py
@@ -4,25 +4,20 @@ nomer = 1
 with open(text_1) as file_1, open(text_2) as file_2:
     for baris1, baris2 in zip(file_1, file_2):
         if baris1.strip() != baris2.strip():
-            kata1 = baris1.strip().lower().split()
-            kata2 = baris2.strip().lower().split()
-            pembanding1 = ""
-            pembanding2 = ""
-            state = False
+            bagian1 = baris1.strip().lower().split()
+            bagian2 = baris2.strip().lower().split()
+            hasil1 = ""
+            hasil2 = ""
+            for kata in bagian1:
+                if kata not in bagian2:
+                    hasil1 += kata + " "
 
-            for kata in kata1:
-                if kata not in kata2:
-                    pembanding1 += kata + " "
-                    state = True
-
-            for kata in kata2:
-                if kata not in kata1:
-                    pembanding2 += kata + " "
-                    state = True
-
-            if state:
+            for kata in bagian2:
+                if kata not in bagian1:
+                    hasil2 += kata + " "
+            if hasil1 or hasil2:
                 print(f"Baris {nomer} ada perbedaan")
                 print(f"File1 = {baris1.strip()}")
                 print(f"File2 = {baris2.strip()}")
-                print(f'File1 ada kata "{pembanding1.strip()}" sedangkan di File2 ada kata "{pembanding2.strip()}"')
+                print(f'File1 ada kata "{hasil1.strip()}" sedangkan di File2 ada kata "{hasil2.strip()}"')
         nomer += 1
