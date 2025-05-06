a1f27a0b5ff58467a3b7cab600892802434f9c6d
Felix Benedictus Setiawan <123315666+CrowZF01@users.noreply.github.com>
2025-05-06 19:50:00 +0700

    Update Minggu11_Tugas_Mandiri_1.3.py

--- Minggu11_Tugas_Mandiri_1.3.py
+++ Minggu11_Tugas_Mandiri_1.3.py
@@ -4,13 +4,11 @@ def kata_unik(nama_file):
         isi = file.read().lower()
         kata_bersih = re.sub(r'[^a-zA-Z\s]', '', isi)
         kata = kata_bersih.split()
-        kata.reverse()
         i = 0
         while i < len(kata):
             if kata.count(kata[i]) > 1:
                 kata.remove(kata[i])
             else:
                 i += 1
-        kata.reverse()
         print('Kata unik:', ', '.join(kata))
 kata_unik('kalimat.txt')
