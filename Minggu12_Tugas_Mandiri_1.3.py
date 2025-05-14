000b6b982b6007d66f7af928f6870fa5cf126c6b
Felix Benedictus Setiawan <123315666+CrowZF01@users.noreply.github.com>
2025-05-14 18:01:48 +0700

    Update Minggu12_Tugas_Mandiri_1.3.py

--- Minggu12_Tugas_Mandiri_1.3.py
+++ Minggu12_Tugas_Mandiri_1.3.py
@@ -3,11 +3,10 @@ fname = input('Masukkan nama file: ')
 with open(fname, 'r') as file:
     membaca = file.read()
     alamat_email = re.findall(r'\S+@\S+', membaca)
-    tempat = {} 
+    tempat = {}
     for email in alamat_email:
         if email in tempat:
             tempat[email] += 1
         else:
             tempat[email] = 1
-    for email, jumlah in tempat.items():
-        print(f'{email}: {jumlah}')
+print(tempat) 
