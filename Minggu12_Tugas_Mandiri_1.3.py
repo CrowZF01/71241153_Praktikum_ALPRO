0e44d4711846ba0e653c1dcb985a7de3d355b62b
Felix Benedictus Setiawan <123315666+CrowZF01@users.noreply.github.com>
2025-05-14 17:55:30 +0700

    Create Minggu12_Tugas_Mandiri_1.3.py

---
+++ Minggu12_Tugas_Mandiri_1.3.py
@@ -0,0 +1,13 @@
+import re
+fname = input('Masukkan nama file: ')
+with open(fname, 'r') as file:
+    membaca = file.read()
+    alamat_email = re.findall(r'\S+@\S+', membaca)
+    tempat = {} 
+    for email in alamat_email:
+        if email in tempat:
+            tempat[email] += 1
+        else:
+            tempat[email] = 1
+    for email, jumlah in tempat.items():
+        print(f'{email}: {jumlah}')
